d68125223f2f79f3ad11d82fdef245f6c7a184b9
Chance Reimer <chance189@knights.ucf.edu>
2019-04-10 14:26:47 -0400

    Adding

--- team09.c
+++ team09.c
@@ -9,3 +9,27 @@ position* team09Move(const enum piece board[][SIZE], enum piece mine, int second
 {
 
 }
+
+
+/*********
+* Returns the best move position available by the team color that is selected
+* Uses min_max recursive principle
+*/
+position* team09_Best_Move(const enum piece board[][SIZE], enum piece mine,  int depth, int alpha, int beta)
+{
+    enum piece tempBoard[SIZE][SIZE]; //Lets initialize a board
+    copy(tempBoard, board); //Copy the board into it
+
+    position* bestMove = team09_init_start_move();
+
+    free(bestMove); //deallocate meme used
+}
+
+//Inits the base move of the Best move function
+position* team09_init_start_move(void)
+{
+    position* move = malloc(sizeof(position));
+    move->x = -1;  //Init to impossible place
+    move->y = -1;
+    return move;
+}
--- team09.h
+++ team09.h
@@ -11,14 +11,22 @@
 **********************************************************
 * We want to find the move that turns the most of the opposite piece's color into our piece.
 * Soln: Use MinMax trees! Given the state of the board (as a const) return the move most likely to cause success!
+* We have to find the length of moves in a direction that gives us the maximum amount of pieces and makes it so the
+* opponent has no valid moves
+* Use alpha beta pruning and min_max
 */
+extern int team09_Min_Max_Depth;
+extern int weight_forfeit, weight_frontier, weight_mobility, weight_stability;
+
+
+
 position* team09Move(const enum piece board[][SIZE], enum piece mine, int secondsleft); //Required function
 
 //Generates the min max tree by simulating all possible outcomes from the current move choice
 //Uses this through min max trees
-position* team09Gen_Min_Max(const enum piece board[][SIZE], enum piece mine, int secondsLeft);
+//position* team09Gen_Min_Max(const enum piece board[][SIZE], enum piece mine, int secondsLeft);
 
-//Returns the best possible move of the generated MinMax Tree
-position* team09_Best_Move(const enum piece board[][SIZE], enum piece mine, int secondsLeft);
-
+//Returns the best possible move by using a min_max
+position* team09_Best_Move(enum piece board[][SIZE], enum piece mine, int depth, int alpha, int beta);
+position* team09_init_start_move(void);
 #endif // TEAM09_H_INCLUDED
